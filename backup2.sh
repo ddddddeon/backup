938531e72c9c8cbfe0310afd1d62eb7976cbb0b5
Chris d'Eon <opfunk@gmail.com>
2014-03-13 02:38:06 -0400

    Add free-space check & tweak prompts

--- backup2.sh
+++ backup2.sh
@@ -6,18 +6,20 @@
 today=`date +_%m_%d_%y`
 
 echo "please enter the directory you want to back up! ABSOLUTE PATH ONLY "
-read backup_files
+read -p "> " backup_files
 
 echo "please enter a destination directory! ABSOLUTE PATH ONLY "
-read dest 
+read -p "> " dest 
+echo "one moment..."
 
 destfree=`df -h $dest | grep / | awk '{ print $4}'`
 destfree2=`df $dest | grep / | awk '{ print $4}'`
 backupsize=`du -hs $backup_files | head -c 4`
 backupsize2=`du $backup_files | awk '{ print $1}'` 
 
+echo ""
 echo "NOTICE: " $backup_files "is" $backupsize "in size, and you have" $destfree "free on the target volume. OK to backup" $backup_files "into" $dest "? (y/n)"
-read yn
+read -p "> " yn
 
 if [[ $yn == [Yy] ]] && [[ $backupsize2 < $destfree2 ]]; then
     cd $dest
